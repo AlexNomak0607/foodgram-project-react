e89b297373f7de4775d224dd57e73b98e7c14adc
AlexNomak0607 <youngbuck0607@mail.ru>
2022-08-30 19:37:04 +0300

    usersfollower_models

---
+++ backend/foodgramm/users/models.py
@@ -0,0 +1,72 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True,
+                              verbose_name='email')
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='username'
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль'
+    )
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name='Фамилия'
+    )
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['first_name',
+                       'last_name',
+                       'username']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follow'
+            )
+        ]
+
+    def __str__(self):
+        return self.author.username
