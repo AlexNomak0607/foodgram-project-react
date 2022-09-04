e9bb31d3cf2ba9a1e897d85c56cfd2203c8f7328
AlexNomak0607 <youngbuck0607@mail.ru>
2022-09-04 14:34:38 +0300

    workflow4

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,7 +11,6 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from users.models import User
@@ -28,13 +27,13 @@ from .serializers import (CreateRecipeSerializer, FavoritesSerializer,
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    pagination_class = PageNumberPagination
+    pagination_class = None
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    pagination_class = PageNumberPagination
+    pagination_class = None
     filter_backends = (filters.SearchFilter, )
     search_fields = ('^name', )
 
